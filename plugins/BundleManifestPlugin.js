f477c438d8e263b33db47c06da5c8ca251d355b8
Shane Osbourne <shane.osbourne8@gmail.com>
2018-03-12 20:39:29 +0000

    remove all logger calls

--- plugins/BundleManifestPlugin.js
+++ plugins/BundleManifestPlugin.js
@@ -3,7 +3,6 @@ const hasha = require('hasha');
 const fs = require('fs');
 
 module.exports = function (bundler) {
-  const logger = bundler.logger;
 
   /**
    * Read the paths already registered within the manifest.json
@@ -12,16 +11,13 @@ module.exports = function (bundler) {
    */
   const readManifestJson = (path) => {
     if (!fs.existsSync(path)) {
-      logger.status('✨', 'create manifest file');
       return {};
     };
-
-    logger.status('🖊', 'update manifest file');
 
     try {
       return JSON.parse(fs.readFileSync(path, 'utf8'));
     } catch(e) {
-      logger.error('manifest file is invalid');
+
       throw e; 
     }
   };
@@ -36,7 +32,6 @@ module.exports = function (bundler) {
     let output = path.join(publicURL, path.basename(bundle.name));
     let input = bundle.entryAsset ? bundle.entryAsset.basename : bundle.assets.values().next().value.basename;
     manifestValue[input] = output;
-    logger.status('✓', `  bundle : ${input} => ${output}`);
     bundle.childBundles.forEach(function (bundle) {
       feedManifestValue(bundle, manifestValue, publicURL);
     });
@@ -49,9 +44,7 @@ module.exports = function (bundler) {
     const manifestPath = path.resolve(dir, 'parcel-manifest.json');
     const manifestValue = readManifestJson(manifestPath);
 
-    logger.status('📦', 'PackageManifestPlugin');
     feedManifestValue(bundle, manifestValue, publicURL);
-    logger.status('📄', `manifest : ${manifestPath}`);
 
     fs.writeFileSync(manifestPath, JSON.stringify(manifestValue));
   });
